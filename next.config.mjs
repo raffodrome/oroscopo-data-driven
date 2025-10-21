4967c6d302e0a2a2b07cabe473d2cddbc940c2a7
raffodrome <raffaelestifini@gmail.com>
2025-10-21 17:12:47 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig =next.config.mjs {
-experimental: { appDir: true }
+const nextConfig = {
+  experimental: { appDir: true }
 };
 export default nextConfig;
